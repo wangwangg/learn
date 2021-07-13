881f2549f916c530605ce289862df3cc419bf062
LAPTOP-RR7MPIFN\wang <1542976270@qq.com>
2021-07-13 23:26:35 +0800

    mock

---
+++ mock-demo/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+import mockjs from 'mockjs'
+
+export default {
+  name: 'App',
+  components: {
+  },
+  created () {
+    var Mock = require('mockjs')
+    var name = Mock.mock('@cname')
+    console.log(name)
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ mock-demo/src/mock/mock.js
@@ -0,0 +1,5 @@
+import mockjs from 'mockjs'
+
+var Mock = require('mockjs')
+var name = Mock.mock('@cname')
+console.log(name)
